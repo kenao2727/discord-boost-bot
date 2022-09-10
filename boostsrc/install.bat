632cdeb643e1f2e6efc93077240404fb524d5844
⇝ Crusions <99071432+crusions@users.noreply.github.com>
2022-09-10 09:57:06 -0500

    Update install.bat

--- boostsrc/install.bat
+++ boostsrc/install.bat
@@ -1,2 +1,6 @@
- pip install -r requirements.txt
-pause
+pip install colorama==0.4.4
+pip install httpx==0.21.3
+pip install requests==2.27.1
+pip install discord==2.0.0
+pip install discord.py==2.0.1
+pip install pycord
