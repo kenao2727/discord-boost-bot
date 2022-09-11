e1e37dd29e83344ff9749e022fdd21f2ce1db788
⇝ Crusions <99071432+crusions@users.noreply.github.com>
2022-09-10 20:21:39 -0500

    Delete install.bat

--- boostsrc/install.bat
+++
@@ -1,7 +0,0 @@
-pip install colorama==0.4.4
-pip install httpx==0.21.3
-pip install requests==2.27.1
-pip install discord==2.0.0
-pip install discord.py==2.0.1
-pip install pycord==2.1.3
-pip install getpass
